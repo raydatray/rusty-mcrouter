6960cd3c1d46280da05dff9bf6d0eafe4f095f5e
Ray Liu <raydatray@gmail.com>
2026-05-12 15:26:21 -0400

    [2/n][protocol] serialize get request into bytes
    Summary:
    
    Test Plan:

--- rusty-mcrouter-protocol/src/request.rs
+++ rusty-mcrouter-protocol/src/request.rs
@@ -1,6 +1,21 @@
-use bytes::Bytes;
+use bytes::{BufMut, Bytes, BytesMut};
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum Request {
     Get { keys: Vec<Bytes> },
+}
+
+impl Request {
+    pub fn serialize_into(&self, out: &mut BytesMut) {
+        match self {
+            Request::Get { keys } => {
+                out.put_slice(b"get");
+                keys.iter().for_each(|k| {
+                    out.put_u8(b' ');
+                    out.put_slice(k);
+                });
+                out.put_slice(b"\r\n");
+            }
+        }
+    }
 }
