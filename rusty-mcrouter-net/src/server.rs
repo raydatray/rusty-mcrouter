3d51691a92863133363fcf511f46642b83772234
Ray Liu <raydatray@gmail.com>
2026-05-22 17:28:51 -0400

    [7/n][commands] impl incr
    Summary:
    
    Test Plan:

--- rusty-mcrouter-net/src/server.rs
+++ rusty-mcrouter-net/src/server.rs
@@ -187,6 +187,7 @@ mod tests {
             | Request::Prepend { .. } => Reply::Stored,
             Request::Get { .. } => Reply::Get { hits: vec![] },
             Request::Delete { .. } => Reply::Deleted,
+            Request::Incr { .. } => Reply::NotFound,
         }
     }
 
@@ -221,6 +222,7 @@ mod tests {
                         .collect(),
                 },
                 Request::Delete { .. } => Reply::Deleted,
+                Request::Incr { .. } => Reply::NotFound,
             }
         }
         let addr = spawn_server(handler).await;
