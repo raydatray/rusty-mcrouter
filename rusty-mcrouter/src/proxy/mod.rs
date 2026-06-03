e57039d037194f3e6d537edf66a7d933275c78ae
Ray Liu <raydatray@gmail.com>
2026-06-03 15:43:01 -0400

    [7/n][threading model] implement proxy thread main
    Summary:
    
    Test Plan:

--- rusty-mcrouter/src/proxy/mod.rs
+++ rusty-mcrouter/src/proxy/mod.rs
@@ -4,6 +4,7 @@ mod handle;
 mod message;
 mod proxy;
 mod proxy_set;
+mod thread;
 mod worker;
 
 pub use config::{ListenerConfig, ProxyThreadConfig, ThreadMode};
@@ -11,4 +12,5 @@ pub use handle::ProxyHandle;
 pub use message::ProxyMessage;
 pub use proxy::Proxy;
 pub use proxy_set::ProxySet;
+pub use thread::proxy_thread_main;
 pub use worker::ConnectionWorker;
