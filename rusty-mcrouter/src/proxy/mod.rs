cb8670016c9dac8ef81f73213ef5978fbff3aed6
Ray Liu <raydatray@gmail.com>
2026-06-03 15:43:01 -0400

    [6/n][threading mode] implement connection worker
    Summary:
    
    Test Plan:

--- rusty-mcrouter/src/proxy/mod.rs
+++ rusty-mcrouter/src/proxy/mod.rs
@@ -4,3 +4,11 @@ mod handle;
 mod message;
 mod proxy;
 mod proxy_set;
+mod worker;
+
+pub use config::{ListenerConfig, ProxyThreadConfig, ThreadMode};
+pub use handle::ProxyHandle;
+pub use message::ProxyMessage;
+pub use proxy::Proxy;
+pub use proxy_set::ProxySet;
+pub use worker::ConnectionWorker;
