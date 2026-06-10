e69586675ec7605d532a22d57c81054041ec68cf
Ray Liu <raydatray@gmail.com>
2026-06-10 00:17:20 -0400

    [10/n][hash routing][refactor] move selectors to submodules
    Summary:
    
    Test Plan:

--- rusty-mcrouter-core/src/lib.rs
+++ rusty-mcrouter-core/src/lib.rs
@@ -1,15 +1,11 @@
-mod ch3;
-mod crc32;
 mod destination_route;
 mod error_route;
-mod furc;
 mod null_route;
 mod pool_route;
 mod route;
 mod route_builder;
-mod salted;
 mod selection_route;
-mod selector;
+mod selectors;
 
 pub use crate::destination_route::DestinationRoute;
 pub use crate::error_route::ErrorRoute;
