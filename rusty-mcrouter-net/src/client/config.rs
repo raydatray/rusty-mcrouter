980f96203ab4362daf369d9a13e335d98d386b70
Ray Liu <raydatray@gmail.com>
2026-07-05 03:18:43 -0400

    [4/n][timeouts][config] add connect/write/reply/read-idle timeout knobs

--- rusty-mcrouter-net/src/client/config.rs
+++ rusty-mcrouter-net/src/client/config.rs
@@ -1,6 +1,12 @@
+use std::time::Duration;
+
 pub struct ClientConfig {
     pub max_pending: usize,
     pub read_buf_initial_capacity: usize,
+    pub connect_timeout: Option<Duration>,
+    pub write_timeout: Option<Duration>,
+    pub reply_timeout: Option<Duration>,
+    pub read_idle_timeout: Option<Duration>,
 }
 
 impl Default for ClientConfig {
@@ -8,6 +14,10 @@ impl Default for ClientConfig {
         Self {
             max_pending: 1024,
             read_buf_initial_capacity: 4096,
+            connect_timeout: Some(Duration::from_millis(1000)),
+            write_timeout: Some(Duration::from_millis(1000)),
+            reply_timeout: Some(Duration::from_millis(1000)),
+            read_idle_timeout: Some(Duration::from_millis(2000)),
         }
     }
 }
