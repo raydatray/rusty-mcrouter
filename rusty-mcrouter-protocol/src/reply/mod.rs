3fb7f7a23407debaa3c6e66a18d3f33b70b15214
Ray Liu <raydatray@gmail.com>
2026-07-14 12:17:02 -0400

    [15/n][protocol] implement top level reply enum
    Summary:
    
    Test Plan:

--- rusty-mcrouter-protocol/src/reply/mod.rs
+++ rusty-mcrouter-protocol/src/reply/mod.rs
@@ -4,3 +4,20 @@ mod delete;
 mod error;
 mod get;
 mod store;
+
+pub use arithmetic::{ArithmeticReply, ArithmeticResult};
+pub use debug::{DebugField, DebugHit, DebugReply};
+pub use delete::DeleteReply;
+pub use error::ErrorReply;
+pub use get::{GetHit, GetReply, RecacheState};
+pub use store::{StoreReply, StoreResult};
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum Reply {
+    Arithmetic(ArithmeticReply),
+    Debug(DebugReply),
+    Delete(DeleteReply),
+    Error(ErrorReply),
+    Get(GetReply),
+    Store(StoreReply),
+}
