fc7fe69d567a8cf8e6618b1f718f624f1ebed6f0
Ray Liu <raydatray@gmail.com>
2026-07-14 11:07:47 -0400

    [8/n][protocol] implement top level request enum
    Summary:
    
    Test Plan:

--- rusty-mcrouter-protocol/src/request/mod.rs
+++ rusty-mcrouter-protocol/src/request/mod.rs
@@ -3,3 +3,35 @@ mod debug;
 mod delete;
 mod get;
 mod store;
+
+use crate::key::Key;
+
+pub use arithmetic::{
+    ArithmeticMode, ArithmeticRequest, ArithmeticTemporalInstruction,
+    ArithmeticTemporalInstructions,
+};
+pub use debug::DebugRequest;
+pub use delete::DeleteRequest;
+pub use get::{GetRequest, GetTemporalInstruction, GetTemporalInstructions};
+pub use store::{StoreMode, StoreRequest};
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum Request {
+    Arithmetic(ArithmeticRequest),
+    Delete(DeleteRequest),
+    Debug(DebugRequest),
+    Get(GetRequest),
+    Store(StoreRequest),
+}
+
+impl Request {
+    pub fn key(&self) -> &Key {
+        match self {
+            Self::Arithmetic(request) => &request.key,
+            Self::Delete(request) => &request.key,
+            Self::Debug(request) => &request.key,
+            Self::Get(request) => &request.key,
+            Self::Store(request) => &request.key,
+        }
+    }
+}
