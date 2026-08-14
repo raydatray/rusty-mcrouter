e78cb7febc7b153d60d3d950902cdb4d7263236c
Ray Liu <raydatray@gmail.com>
2026-08-14 19:00:38 -0400

    [28/n][net] make the pool gate decision an enum

--- rusty-mcrouter-net/src/tko/mod.rs
+++ rusty-mcrouter-net/src/tko/mod.rs
@@ -7,5 +7,5 @@ mod tracker;
 pub use counters::TkoCounters;
 pub use events::{default_sink, TkoEvent, TkoEventRecord, TkoEventSink};
 pub use map::TkoTrackerMap;
-pub use pool::{FailOpenThresholds, PoolTkoTracker};
+pub use pool::{FailOpenThresholds, GateDecision, PoolTkoTracker};
 pub use tracker::{DestToken, TkoTracker};
