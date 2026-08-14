4a5eec82d99c7ed9cb9c9ba447b806c688ec461c
Ray Liu <raydatray@gmail.com>
2026-08-14 19:00:04 -0400

    [21/n][net] implement destination map and idle sweep

--- rusty-mcrouter-net/src/destination/mod.rs
+++ rusty-mcrouter-net/src/destination/mod.rs
@@ -1,10 +1,12 @@
 mod config;
 mod destination;
 mod key;
+mod map;
 mod probe;
 mod stats;
 
 pub use config::Config;
 pub use destination::Destination;
 pub use key::Key;
+pub use map::Map;
 pub use stats::Stats;
